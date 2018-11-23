58dad1bd4b5952d8f8979e8be2163d5fbe21dc79
Kjue <kelvinjue@gmail.com>
2018-11-22 17:59:45 -0800

    Updates

---
+++ precache-manifest.0ddb573a552103467227f31886d0049f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a7404af45b9b04dd9f10eb13ff32409",
+    "url": "/personal-website-v2/static/media/teacher-portal-2.6a7404af.png"
+  },
+  {
+    "revision": "db56e287bc020548c150",
+    "url": "/personal-website-v2/static/css/main.f9f95902.chunk.css"
+  },
+  {
+    "revision": "53b1b005de02b6d05c5c",
+    "url": "/personal-website-v2/static/js/1.53b1b005.chunk.js"
+  },
+  {
+    "revision": "bdbefb8a60e27be83822",
+    "url": "/personal-website-v2/static/js/runtime~main.bdbefb8a.js"
+  },
+  {
+    "revision": "b68117a3e5382dc0cc34895c75b026fa",
+    "url": "/personal-website-v2/static/media/logo.b68117a3.svg"
+  },
+  {
+    "revision": "926d1c907da68b36be5dc19a25179f66",
+    "url": "/personal-website-v2/static/media/linkedin.926d1c90.svg"
+  },
+  {
+    "revision": "45367bbb839281ecd36ef00e2bb4b7aa",
+    "url": "/personal-website-v2/static/media/github.45367bbb.svg"
+  },
+  {
+    "revision": "75e45ea827c52eb604e5a197e9d7c964",
+    "url": "/personal-website-v2/static/media/contract-1.75e45ea8.svg"
+  },
+  {
+    "revision": "671627af8a8432e40722ca9fd84567c5",
+    "url": "/personal-website-v2/static/media/ikea_monkey.671627af.png"
+  },
+  {
+    "revision": "db56e287bc020548c150",
+    "url": "/personal-website-v2/static/js/main.db56e287.chunk.js"
+  },
+  {
+    "revision": "677b6191026d04c93377daf47f764216",
+    "url": "/personal-website-v2/static/media/where2meet.677b6191.png"
+  },
+  {
+    "revision": "9585d1c681c3cc6ca8ee0d7cc2e83e38",
+    "url": "/personal-website-v2/static/media/cny.9585d1c6.jpeg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/personal-website-v2/static/media/react.05a3e30f.svg"
+  },
+  {
+    "revision": "fbef321e03c16bcae42560f8507757d6",
+    "url": "/personal-website-v2/static/media/python.fbef321e.svg"
+  },
+  {
+    "revision": "228de75abd14d99573ffb396c2ecf014",
+    "url": "/personal-website-v2/static/media/java.228de75a.svg"
+  },
+  {
+    "revision": "45e46bc025b8fd350c09492ce01d4549",
+    "url": "/personal-website-v2/static/media/postgresql.45e46bc0.svg"
+  },
+  {
+    "revision": "6d0000a6bc985581ae3b114e9fcfb24a",
+    "url": "/personal-website-v2/static/media/c.6d0000a6.svg"
+  },
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "/personal-website-v2/static/media/AvenirNextLTPro-Regular.b5ed9330.otf"
+  },
+  {
+    "revision": "c83ad54c345963921fe8f6dad5240492",
+    "url": "/personal-website-v2/index.html"
+  }
+];
