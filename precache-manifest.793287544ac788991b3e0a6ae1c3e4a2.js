e83e3b1320b6d18e6dec3fe24774b9aef1be1456
Kjue <kelvinjue@gmail.com>
2018-11-22 22:32:16 -0800

    Updates

--- precache-manifest.793287544ac788991b3e0a6ae1c3e4a2.js
+++ precache-manifest.793287544ac788991b3e0a6ae1c3e4a2.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/personal-website-v2/static/media/teacher-portal-2.6a7404af.png"
   },
   {
-    "revision": "81bea956f74237bb65f4",
+    "revision": "518c4c3d2b8b6bfee071",
     "url": "/personal-website-v2/static/css/main.fdfbbc6d.chunk.css"
   },
   {
@@ -36,8 +36,8 @@ self.__precacheManifest = [
     "url": "/personal-website-v2/static/media/ikea_monkey.671627af.png"
   },
   {
-    "revision": "81bea956f74237bb65f4",
-    "url": "/personal-website-v2/static/js/main.81bea956.chunk.js"
+    "revision": "518c4c3d2b8b6bfee071",
+    "url": "/personal-website-v2/static/js/main.518c4c3d.chunk.js"
   },
   {
     "revision": "677b6191026d04c93377daf47f764216",
@@ -72,7 +72,7 @@ self.__precacheManifest = [
     "url": "/personal-website-v2/static/media/AvenirNextLTPro-Regular.b5ed9330.otf"
   },
   {
-    "revision": "4dcc25688e2d336e55358f996cb88ece",
+    "revision": "cd472fe57b28b03840a0165bf167f086",
     "url": "/personal-website-v2/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/personal-website-v2/precache-manifest.eef61c1db4ccba1d49d7e10fe8145075.js"
+  "/personal-website-v2/precache-manifest.793287544ac788991b3e0a6ae1c3e4a2.js"
 );
 
 workbox.clientsClaim();
