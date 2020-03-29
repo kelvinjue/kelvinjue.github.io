89fb8093987388cddd518a9d397f6ea0a7c40bd8
Kelvin Jue <kelvinjue@berkeley.edu>
2020-03-29 02:22:48 -0500

    Updates

---
+++ precache-manifest.4c8e10fe2363a8b5ef708162956def92.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8472b0b1ea77994c76462aaf9452e231",
+    "url": "/static/media/logo.8472b0b1.svg"
+  },
+  {
+    "revision": "abf6b5a1f0a041570859c3c5e59b1880",
+    "url": "/static/media/linkedin-black.abf6b5a1.png"
+  },
+  {
+    "revision": "2de6fb3a75c0ba5e1f319c95aaee62d9",
+    "url": "/static/media/kelvin.2de6fb3a.jpg"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/static/media/github-black.07883e93.svg"
+  },
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "/static/media/AvenirNextLTPro-Regular.b5ed9330.otf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "49de63e93cc64a714bdb",
+    "url": "/static/js/main.49de63e9.chunk.js"
+  },
+  {
+    "revision": "898a9d31f7ae1116b1ba",
+    "url": "/static/js/1.898a9d31.chunk.js"
+  },
+  {
+    "revision": "49de63e93cc64a714bdb",
+    "url": "/static/css/main.2158155b.chunk.css"
+  },
+  {
+    "revision": "6e0729e8453d9ea2d4e8171cd72daf25",
+    "url": "/index.html"
+  }
+];
